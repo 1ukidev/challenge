2c0bf380cc0ec82054c3496118b0ed3d5fe4fcb0
Leonardo / LuKi <leo.monteiro06@live.com>
2022-03-08 21:12:11 -0300

    Arquivos iniciais

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const btnAdicionar = document.getElementById("adicionar");
+const btnRemover = document.getElementById("remover");
+const lista = document.getElementById("lista");
+const texto = document.getElementById("texto");
+
+btnAdicionar.onclick = () => {
+    const entrada = document.createElement("li");
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+
+    const textnode = document.createTextNode(texto.value);
+
+    if(texto.value == "" || !/\S/.test(texto.value)) {
+        return null;
+    } else {
+        entrada.appendChild(checkbox);
+        entrada.appendChild(textnode);
+        lista.appendChild(entrada);
+    }
+};
+
+btnRemover.onclick = () => {
+    const items = [].slice.call(lista.childNodes);
+    let item;
+
+    while(item = items.pop()) {
+        if(item.firstChild && item.firstChild.checked) {
+            lista.removeChild(item);
+        }
+    };
+};
